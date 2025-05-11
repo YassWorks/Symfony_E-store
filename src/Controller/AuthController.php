f40727adb986c791c788eeb503bb5b8e4023d86a
YassWorks <mohamedyassine.kallel@insat.ucar.tn>
2025-05-11 21:22:56 +0100

    basic pages added

--- src/Controller/AuthController.php
+++ src/Controller/AuthController.php
@@ -7,10 +7,22 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class AuthController extends AbstractController
-{
-    #[Route('/', name: 'app_auth')]
+{   
+    #[Route('/', name: 'landing_page')]
+    public function landing(): Response
+    {
+        return $this->render('home/index.html.twig');
+    }
+
+    #[Route('/login', name: 'app_login')]
     public function index(): Response
     {
         return $this->render('auth/index.html.twig');
+    }
+
+    #[Route('/register', name: 'app_register')]
+    public function register(): Response
+    {
+        return $this->render('auth/register.html.twig');
     }
 }
